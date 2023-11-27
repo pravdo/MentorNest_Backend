48f7c183c557d07c149e6600ecde9e2906e8004d
Pravdomir Kostov <pravdomir1998@gmail.com>
2023-11-28 00:31:10 +0200

    input validation & transformation

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateCourseDto {
+  @IsString()
+  @MinLength(5)
   @ApiProperty()
   name: string;
 
+  @IsString()
+  @MinLength(25)
   @ApiProperty()
   description: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,21 +1,45 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '@prisma/client';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   @ApiProperty()
   userName: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   @ApiProperty()
   password: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   @ApiProperty()
   firstName: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
   @ApiProperty()
   lastName: string;
 
+  @IsEnum(UserRole)
+  @IsNotEmpty()
   @ApiProperty({ required: false, enum: ['ADMIN', 'BASIC'], default: 'BASIC' })
   role: string = 'BASIC';
 }
